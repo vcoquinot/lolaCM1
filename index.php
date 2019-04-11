4fd62aee82ceda94b706c282ef1e6946eff1350d
vcoquinot <vcoquinot@gmail.com>
2019-04-11 17:41:48 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,86 @@
+<html>
+  <head>
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/tuto.css" rel="stylesheet">
+      <style type="text/css">
+    .col-lg-8 { line-height: 200px; }
+    .col-lg-12 { line-height: 80px; }
+    .centered-column{float: none;margin: 0 auto;}
+  </style>
+  </head>
+  <body>
+  	<div class="container">
+      <h1> Mets au passé simple</h1>
+      <div class="form-group">
+        <div label for="1">1. Il (prendre) son parapluie.</div>
+          <select>
+            <option>prenat</option>
+            <option>prit</option>
+            <option>prenit</option>
+          </select>
+      </div>
+      <div class="form-group">
+        <div label for="2">2. Je (marcher) sous la pluie.</div>
+          <select>
+            <option>marchais</option>
+            <option>marchai</option>
+            <option>marche</option>
+          </select>
+        </div>
+      <div class="form-group">
+        <div label for="3">3. Nous (chanter) sous la pluie.</div>
+          <select>
+            <option>chantâmes</option>
+            <option>chamtames</option>
+            <option>chantame</option>
+          </select>
+      </div>
+      <div class="form-group">
+        <div label for="4">4. Ils (tenir) leur parapluie.</div>
+          <select>
+            <option>tenèrent</option>
+            <option>tenurent</option>
+            <option>tinrent</option>
+          </select>
+      </div>
+      <div class="form-group">
+        <div label for="5">5. Tu (mourrir) sous la pluie.</div>
+          <select>
+            <option>mourrus</option>
+            <option>mourris</option>
+            <option>mourus</option>
+          </select>
+      </div>
+      <div class="form-group">
+        <div label for="6">6. Vous (penser) la même chose.</div>
+          <select> 
+            <option>pensâte</option>
+            <option>pensattes</option>
+            <option>pensâtes</option>
+          </select>
+      </div>
+      <div class="form-group">
+        <div label for="7">7. Ils (écouter) attentivement.</div>
+          <select>
+            <option>écouturent</option>
+            <option>écoutèrent</option>
+            <option>écoutirent</option>
+          </select>
+      </div>
+
+      <div class="form-group">
+        <div label for="8">8. Il (faire) la moue.</div>
+          <select>
+            <option>fait</option>
+            <option>fire</option>
+            <option>fit</option>
+          </select>
+      </div>
+
+      <?php echo "Ceci est du texte"; ?>
+      <div class="form-group">
+        <button type="submit" class="btn btn-primary">Valider mes réponses</button>
+      </div>
+    </div>
+  </div>
+
